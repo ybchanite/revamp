e7c10efa8adc30261aad0142d05af7dd765da2d2
ybchanite <33406167+ybchanite@users.noreply.github.com>
2018-01-03 14:23:00 +0800

    PHD

--- bootstrap_cityu/cityu/cityu.js
+++ bootstrap_cityu/cityu/cityu.js
@@ -670,18 +670,16 @@ jQuery('#block-views-block-research-areas-block-1 .view_research_areas .view_res
 */
 
 
-jQuery('#block-researchphdfellowshipscheme201718 .paragraph--type--contents-with-menu .field--name-field-caption p:nth-child(-n+13):not(:nth-child(12))').click(function(){
-        var index = jQuery("#block-researchphdfellowshipscheme201718 .paragraph--type--contents-with-menu .field--name-field-caption p").index(jQuery(this)) + 1;
+jQuery('#block-researchphdfellowshipscheme201718 .paragraph--type--contents-with-menu .col-sm-2 p:nth-child(-n+13):not(:nth-child(12))').click(function(){
+        var index = jQuery("#block-researchphdfellowshipscheme201718 .paragraph--type--contents-with-menu .col-sm-2 p").index(jQuery(this)) + 1;
         
         /*For exceptional case as the 12th list item is a hyperlink with no corresponding description block*/
         if (index === 12) {index = -1;}
         if (index === 13) { index--;}
         
         
-        jQuery('.landing_research_phdfscheme > .col-sm-10 div').css('display','none');
-        
         var target = jQuery("#block-researchphdfellowshipscheme201718 .field--name-field-body .field--items:nth-child(" + index + ")");
-	jQuery('#block-researchphdfellowshipscheme201718 .field--name-field-body .field--items').css('display','none');
+	jQuery('#block-researchphdfellowshipscheme201718 .col-sm-10 .field--items').css('display','none');
         target.show();
         
          
